994e204ad263d5d6b89683d7aa8f56e13168dd12
Timmy Babayeju <oktbabs@gmail.com>
2019-11-01 08:39:51 -0700

    Revert "Added feature 2"
    This reverts commit 518bd5e1cf614687f028d99224e54ba0319df716.
    
    Conflicts:
    	main.c

--- main.c
+++ main.c
@@ -7,10 +7,13 @@ print "Hello World"
 }
 Added security patch to master
 
+<<<<<<< HEAD
 Added Feature1
 
 Added security Feature 2
 
+=======
+>>>>>>> parent of 518bd5e... Added feature 2
 Initial Security()
 {
  //Sample Code
